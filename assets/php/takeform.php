45ebee6bf3805d3c4bb589f67c60553372316be4
MrPringMrles <greensparkles808@gmail.com>
2017-09-10 23:49:33 -1000

    Update takeform.php

--- assets/php/takeform.php
+++ assets/php/takeform.php
@@ -1,48 +1,61 @@
-<html>
-<head>
-<title>Thanks For Contacting Me</title>
-</head>
-<body>
 <?php
-  // Change this to YOUR address
-  $recipient = 'greensparkles808@gmail.com';
-  $email = $_POST['email'];
-  $realName = $_POST['realname'];
-  $body = $_POST['body'];
-  $messages = array();
-# Fake email filter chars
-if (!preg_match("/^[\w\+\-.~]+\@[\-\w\.\!]+$/", $email)) {
-$messages[] = "That is not a valid email address.";
+if(!isset($_POST['submit']))
+{
+    //This page should not be accessed directly. Need to submit the form.
+    echo "error; you need to submit the form!";
 }
-# Fake name filter chars
-if (!preg_match("/^[\w\ \+\-\'\"]+$/", $realName)) {
-$messages[] = "The real name field must contain only " .
-"alphabetical characters, numbers, spaces, and " .
-"reasonable punctuation. We apologize for any inconvenience.";
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
 }
 
-$body = $_POST['body'];
-# Make sure the message has a body
-if (preg_match('/^\s*$/', $body)) {
-$messages[] = "Your message was blank. Did you mean to say " .
-"something?"; 
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
 }
-  if (count($messages)) {
-    # There were problems, so tell the user and
-    # don't send the message yet
-    foreach ($messages as $message) {
-      echo("<p>$message</p>\n");
-    }
-    echo("<p>Click the back button and correct the problems. " .
-      "Then click Send Your Message again.</p>");
-  } else {
-    # Send the email - we're done
-mail($recipient,
-      $body,
-      "From: $realName <$email>\r\n" .
-      "Reply-To: $realName <$email>\r\n"); 
-    echo("<p>Your message has been sent. Thank you!</p>\n");
+
+$email_from = 'tom@amazing-designs.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
+
+$to = "greensparkles808@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
   }
-?>
-</body>
-</html>
+  else
+    {
+    return false;
+  }
+}
+
+?> 
